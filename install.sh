5e0c3854bba6ba2118eafa2718af591146912105
Aitor Iturrioz <riturrioz@gmail.com>
2018-06-01 20:41:33 +0200

    Add the 'FLUSH PRIVILEGES;' command to the database configuration (closes #1)

--- install.sh
+++ install.sh
@@ -88,6 +88,10 @@ mysql -u $mysql_root_user -p$mysql_root_password -Bse "CREATE USER '$mysql_openh
 # Give openhab user enough permissions
 mysql -u $mysql_root_user -p$mysql_root_password -Bse "GRANT ALL PRIVILEGES ON $mysql_database_name.* TO '$mysql_openhab_user'@'localhost';" > /dev/null 2>&1
 
+# Flush privileges
+mysql -u $mysql_root_user -p$mysql_root_password -Bse "FLUSH PRIVILEGES;" > /dev/null 2>&1
+
+
 echo -e "MySQL database correctly configured\n"
 
 
